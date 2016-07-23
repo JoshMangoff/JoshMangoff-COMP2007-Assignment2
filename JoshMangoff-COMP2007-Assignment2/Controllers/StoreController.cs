54aa181df19304556dc5790e215c50b292b4150e
Josh Mangoff <jmangoff@hotmail.com>
2016-07-23 16:24:30 -0400

    Updated Controllers a bit

---
+++ JoshMangoff-COMP2007-Assignment2/Controllers/StoreController.cs
@@ -0,0 +1,32 @@
+﻿using JoshMangoff_COMP2007_Assignment2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JoshMangoff_COMP2007_Assignment2.Controllers
+{
+    public class StoreController : Controller
+    {
+        // GET: Store
+        public string Index()
+        {
+            return "List of food types goes here";
+        }
+
+        //GET: Store/Browse?type=Dessert
+        public string Browse(string type = "Dessert")
+        {
+            string message = HttpUtility.HtmlEncode("List of food of " + type +" goes here");
+            return message;
+        }
+
+        // GET: Store/Details/2
+        public ActionResult Details(int id = 1)
+        {
+            Food food = new Food(id);
+            return View(food);
+        }
+    }
+}
---
+++ JoshMangoff-COMP2007-Assignment2/Models/Food.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JoshMangoff_COMP2007_Assignment2.Models
+{
+    public class Food
+    {
+
+        public Food()
+        {
+
+        }
+        
+        public Food(string Title, string Description, int Price, string SmallImgUrl, string BigImgUrl)
+        {
+            this.Title = Title;
+            this.Description = Description;
+            this.Price = Price;
+            this.SmallImgUrl = SmallImgUrl;
+            this.BigImgUrl = BigImgUrl;
+        }
+
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public int Price { get; set; }
+        public string SmallImgUrl { get; set; }
+        public string BigImgUrl { get; set; }
+    }
+}
